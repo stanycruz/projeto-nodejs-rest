6a038ce76c4599de96e210aca04a00528cd906d4
Stanislaw Cruz <stanycruz@outlook.com>
2020-07-20 22:15:38 -0300

    Refatorando o código.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,17 +1,45 @@
+const moment = require('moment');
 const conexao = require('../infrastructure/conexao');
-const atendimentos = require('../controllers/atendimentos');
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?';
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if (erro) {
-                console.log(erro);
-            } else {
-                console.log(resultados);
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        });
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(resultados);
+                }
+            });
+        }
     }
 }
 
